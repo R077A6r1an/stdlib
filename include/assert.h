0cd7bb5abecff1a0b7704464f6f4551e50712cab
Adrian Ross <ross.adrian006@gmail.com>
2023-07-31 21:20:36 -0400

    Fixing a false comment that wasnt up-to-date

--- include/assert.h
+++ include/assert.h
@@ -3,6 +3,7 @@
 
 // for NULL
 #include <stddef.h>
+#include <stdio.h>
 
 //FIXME: this is not really what assert should do, but in a freestanding environment it is a bit questionable to end the programm
 
@@ -12,7 +13,11 @@
                       asm volatile("cli; hlt"); \
                     }
 #else
-#  define assert(x) x == 0
+// if you dont want to terminate the process, assert will just print a error message.
+// Normaly on stderr, but fprintf currently has issues.
+#  define assert(x) if(x == 0) { \
+                      printf("Assertion failed: function %s, file %s, line %s\n", __func__, __FILE__, __LINE__); \
+                    }
 #endif
 
 #define static_assert _Static_assert
